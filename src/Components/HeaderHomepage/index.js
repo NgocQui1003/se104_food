36d3f5374eb4ab3ecf104c95634857aa91ad2877
unknown <18520298@gm.uit.edu.vn>
2021-11-04 15:22:01 +0700

    UPDATE: fix responsive Header Homepage

--- src/Components/HeaderHomepage/index.js
+++ src/Components/HeaderHomepage/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './HeaderHomepage.scss';
+import styles from "./HeaderHomepage.module.scss";
 
 import MenuIcon from '@mui/icons-material/Menu';
 import CancelIcon from '@mui/icons-material/Cancel';
 
 import logo from "../../Assets/100x100.png";
+import { style } from "@mui/system";
 function Navbar() {
     const [click, setClick] = useState(false);
     const [navbar, setNavbar] = useState(false);
@@ -24,67 +25,67 @@ function Navbar() {
 
     return (
         <>
-            <nav className={navbar ? 'navbar active' : 'navbar'}>
-                <div className='navbar-container'>
-                    <div className='navbar-col'>
-                        <ul className='nav-menu'>
-                            <li className='nav-item'>
-                                <Link to='/tim-kiem-cong-thuc' className='nav-links' onClick={closeMobileMenu}>
+            <nav className={navbar ? [styles['navbar'], styles['active']].join(' ') : styles['navbar']}>
+                <div className={styles['navbar-container']}>
+                    <div className={styles['navbar-col']}>
+                        <ul className={styles['nav-menu']}>
+                            <li className={styles['nav-item']}>
+                                <Link to='/tim-kiem-cong-thuc' className={styles['nav-links']} onClick={closeMobileMenu}>
                                     Tìm kiếm công thức
                                 </Link>
                             </li>
-                            <li className='nav-item'>
-                                <Link to='/lien-he' className='nav-links' onClick={closeMobileMenu}>
+                            <li className={styles['nav-item']}>
+                                <Link to='/lien-he' className={styles['nav-links']} onClick={closeMobileMenu}>
                                     Liên hệ
                                 </Link>
                             </li>
                         </ul>
                     </div>
 
-                    <div className='navbar-col'>
+                    <div className={styles['navbar-col']}>
                         <Link to='/' className='navbar-logo'>
                             <img src={logo} />
                         </Link>
                     </div>
 
-                    <div className='navbar-col'>
-                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                            <div className={click ? 'nav-item-remove' : 'nav-item-remove active'}>
-                                <li className='nav-item'>
-                                    <Link to='/random-mon-an' className='nav-links' onClick={closeMobileMenu}>
+                    <div className={styles['navbar-col']}>
+                        <ul className={click ? [styles['nav-menu'], styles['active']].join(' ') : styles['nav-menu']}>
+                            <div className={click ? styles['nav-item-remove'] : [styles['nav-item-remove'], styles['active']].join(' ')}>
+                                <li className={styles['nav-item']}>
+                                    <Link to='/random-mon-an' className={styles['nav-links']} onClick={closeMobileMenu}>
                                         Random món ăn
                                     </Link>
                                 </li>
-                                <li className='nav-item'>
-                                    <Link to='/tim-kiem-cong-thuc' className='nav-links' onClick={closeMobileMenu}>
+                                <li className={styles['nav-item']}>
+                                    <Link to='/tim-kiem-cong-thuc' className={styles['nav-links']} onClick={closeMobileMenu}>
                                         Tìm kiếm công thức
                                     </Link>
                                 </li>
-                                <li className='nav-item'>
-                                    <Link to='/don-tu-lanh' className='nav-links' onClick={closeMobileMenu}>
+                                <li className={styles['nav-item']}>
+                                    <Link to='/don-tu-lanh' className={styles['nav-links']} onClick={closeMobileMenu}>
                                         Dọn tủ lạnh
                                     </Link>
                                 </li>
-                                <li className='nav-item'>
-                                    <Link to='/lien-he' className='nav-links' onClick={closeMobileMenu}>
+                                <li className={styles['nav-item']}>
+                                    <Link to='/lien-he' className={styles['nav-links']} onClick={closeMobileMenu}>
                                         Liên hệ
                                     </Link>
                                 </li>
                             </div>
-                            <li className='nav-item'>
-                                <Link to='/dang-nhap' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            <li className={styles['nav-item']}>
+                                <Link to='/dang-nhap' className={styles['nav-links-mobile']} onClick={closeMobileMenu}>
                                     Đăng nhập
                                 </Link>
                             </li>
-                            <li className='nav-item'>
-                                <Link to='/dang-ki' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            <li className={styles['nav-item']}>
+                                <Link to='/dang-ki' className={styles['nav-links-mobile']} onClick={closeMobileMenu}>
                                     Đăng kí
                                 </Link>
                             </li>
                         </ul>
                     </div>
 
-                    <div className='menu-icon' onClick={handleClick}>
+                    <div className={styles['menu-icon']} onClick={handleClick}>
                         {click ? <CancelIcon fontSize="large" /> : <MenuIcon fontSize="large" />}
                     </div>
                 </div>
