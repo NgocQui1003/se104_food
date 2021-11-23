822cba1fae86a313454e3da389e558b1257fcd2e
unknown <18520298@gm.uit.edu.vn>
2021-11-23 11:23:01 +0700

    CREATE: contact page

---
+++ src/Pages/Contact/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div className={styles['container']}>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
