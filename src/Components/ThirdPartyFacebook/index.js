a4af5a4caac5826f616f4e578b2940eef58f5ffd
hoangnhut <hoangnhutsp@gmail.com>
2021-12-09 10:43:06 +0700

    FIX: Facebook, Google login

--- src/Components/ThirdPartyFacebook/index.js
+++ src/Components/ThirdPartyFacebook/index.js
@@ -2,20 +2,32 @@ import React from 'react'
 import styles from './ThirdPartyFacebook.module.scss';
 import FacebookIcon from '@mui/icons-material/Facebook';
 
-import { ReactDOM } from 'react-dom';
 import FacebookLogin from 'react-facebook-login';
 import thirdpartyApi from '../../Api/thirdpartyApi';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
+import userApi from '../../Api/userApi';
+import Auth from '../../Utils/Auth';
 
+import { userActions } from '../../Redux/Actions/userActions';
 
 function ThirdPartyFacebook() {
+    const history = useHistory();
     const dispatch = useDispatch();
 
     const responseFacebook = async (response) => {
+        if (response.status == 'unknown') return;
         let accessToken = response.accessToken;
         let userID = response.userID;
-        const res = await dispatch(thirdpartyApi.requestFacebook({ accessToken, userID }));
+        console.log(response);
+        const data = await thirdpartyApi.requestFacebook({ accessToken, userID });
+        if (data.success) {
+            Auth.setToken(data.accessToken)
+            const res = await userApi.getProfile();
+            dispatch(userActions.setProfile(res.data))
+            history.push('/');
+        }   
     }
 
     return (
--- src/Components/ThirdPartyGoogle/index.js
+++ src/Components/ThirdPartyGoogle/index.js
@@ -1,39 +1,43 @@
 import React from 'react'
 import styles from './ThirdPartyGoogle.module.scss';
-import GoogleIcon from '@mui/icons-material/Google';
 
 import GoogleLogin from 'react-google-login';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import thirdpartyApi from '../../Api/thirdpartyApi';
+import userApi from '../../Api/userApi';
+import Auth from '../../Utils/Auth';
 
+import { userActions } from '../../Redux/Actions/userActions';
 function ThirdPartyGoogle() {
-
+    const history = useHistory();
     const dispatch = useDispatch();
-    const responseSuccessGoogle = (response) => {
+    const responseSuccessGoogle = async (response) => {
         let tokenId = response.tokenId
-        dispatch(thirdpartyApi.requestGoogle(tokenId));
-        alert('Đăng nhập bằng Google thành công');
+        const data = await thirdpartyApi.requestGoogle(tokenId);
+        
+        if (data.success) {
+            Auth.setToken(data.accessToken)
+            const res = await userApi.getProfile();
+            dispatch(userActions.setProfile(res.data))
+            history.push('/');
+        }        
     }
 
     const responseErrorGoogle = (res) => {
         console.log(res);
-        alert('Đăng nhập bằng Google thất bại');
+        // alert('Đăng nhập bằng Google thất bại');
     }
 
 
     return (
         <div className={styles['container']}>
-            {/* <div className={styles['btn-login-google']}>
-                <GoogleIcon /> 
-                <span>Login with Google</span>
-            </div> */}
-
             <GoogleLogin
                 clientId="811148561616-u5o162igd4bdqkb26lan40e7t356hh7f.apps.googleusercontent.com"
                 buttonText="Đăng nhập bằng Google"
-                onSuccess={responseSuccessGoogle}
-                onFailure={responseErrorGoogle}
+                onSuccess={(e) => responseSuccessGoogle(e)}
+                onFailure={responseErrorGoogle()}
                 cookiePolicy={'single_host_origin'}
                 className='btn-login-google'
             />
